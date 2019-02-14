feba27ee4997ea22b6c6241365b16f3b0e02ee27
Bejofo <32919220+Bejofo@users.noreply.github.com>
2019-02-14 18:42:42 +0800

    bug fix

--- server.js
+++ server.js
@@ -17,11 +17,10 @@ wss.on('connection', function connection(ws) {
   console.log('WS opened');
   ws.on('message', function incoming(data) {
     console.log(data);
-    // Broadcast to everyone else.
-	if(message.split('|')[0] === process.env.key){
+	if(data.split('|')[0] === process.env.key){
     wss.clients.forEach(function each(client) {
       if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message.split('|')[1]);
+        client.send(data.split('|')[1]);
       }
     });
   }
