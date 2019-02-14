603b18af1f12fa62ceef49460c5f446e07d94ea6
Bejofo <32919220+Bejofo@users.noreply.github.com>
2019-02-14 18:30:49 +0800

    lets see if this works

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const WebSocket = require('ws');
+const cheerio = require('cheerio');
+
+const SocketServer = WebSocket.Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', function connection(ws) {
+  console.log('WS opened');
+  ws.on('message', function incoming(data) {
+    console.log(data);
+    // Broadcast to everyone else.
+	if(message.split('|')[0] === process.env.key){
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message.split('|')[1]);
+      }
+    });
+  }
+  });
+});
